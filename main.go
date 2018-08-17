01435e98531789c9d6f32c674ba174dbcc10b510
Keshav Kumar <keshavkumar61996@gmail.com>
2018-08-17 20:21:26 +0530

    writing deck to file 27

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
 func main() {
+	//var card string = "Ace of Diamonds"
+	cards := newDeck()
 
+	hand, remaningCards := deal(cards, 5)
+
+	hand.print()
+	remaningCards.print()
+	hand.saveToFile("hand")
+	remaningCards.saveToFile("remainingCards")
 }
